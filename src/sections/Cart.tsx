795ce33569bfd09506d9340d4565ec58aa23f757
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2023-05-11 16:33:19 -0300

    feat: initialize project

---
+++ src/sections/Cart.tsx
@@ -0,0 +1,17 @@
+import { VStack } from "@chakra-ui/react";
+import React from "react";
+
+const Cart = () => {
+  return (
+    <VStack
+      w="full"
+      h="full"
+      p="10"
+      spacing="10"
+      alignItems="flex-start"
+      bg="green"
+    ></VStack>
+  );
+};
+
+export default Cart;
---
+++ src/sections/Details.tsx
@@ -0,0 +1,79 @@
+import {
+  Heading,
+  VStack,
+  Text,
+  SimpleGrid,
+  GridItem,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Checkbox,
+  Button,
+} from "@chakra-ui/react";
+import React from "react";
+
+const Details = () => {
+  return (
+    <VStack
+      w="full"
+      h="full"
+      p="10"
+      spacing="10"
+      alignItems="flex-start"
+      bg="gray.50"
+    >
+      <VStack spacing="3" alignItems="flex-start">
+        <Heading size="2xl">Your details</Heading>
+        <Text>If you already have an account, click here to log in</Text>
+      </VStack>
+      <SimpleGrid columns={2} columnGap="3" rowGap="6" w="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="John" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Doe" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Adress</FormLabel>
+            <Input placeholder="Rua dos Bobos, 0" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="Rio Claro" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Select>
+              <option value="brl">Brasil</option>
+              <option value="col">Colômbia</option>
+              <option value="arg">Argentina</option>
+              <option value="uru">Uruguai</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to billing address.</Checkbox>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full">
+            Place Order
+          </Button>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
+  );
+};
+
+export default Details;
